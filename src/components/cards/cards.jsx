2a62832c04457483969bd3f1e697fcb705825076
Dilara <dilaraozturk028@gmail.com>
2025-05-19 11:20:25 +0200

    modif boissons

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,8 +1,17 @@
 import './cards.css'
-
+import Limonade from '../../assets/Limonade.png'
+import orange from '../../assets/orange.png'
+import vert from '../../assets/vert.png'
+import Tea from '../../assets/Tea.png'
 function Cards() {
     return (
     <section id='card'>
+        <div id='img-card'>
+            <img src={Limonade} alt="" />
+            <img src={orange} alt="" />
+            <img src={vert} alt="" id='vert'/>
+            <img src={Tea} alt="" id='tea'/>
+        </div>
     </section>
     );
 }
